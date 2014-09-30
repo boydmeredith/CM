d2ca5172c89c1a0b7ca667a5a0db13499a3de199
tyler boyd <jboydmeredith@gmail.com>
2014-09-29 21:08:16 -0700

    figure builder works

--- CM_figureBuilder.m
+++ CM_figureBuilder.m
@@ -2,32 +2,36 @@ function CM_figureBuilder()
 trs_to_use = 1:6;
 res_dir = '/Volumes/Tyler_Drive1/CM_mvpa_results';
 %first, let's look at ex>ex at each tr
-res_str = 'conds_hitVcr_trTe1_2_3_4_0_0_0_0_trW';
-tr_str = {'0__0__0__0__0__1' '0__0__0__0__1__0' '0__0__0__1__0__0' '0__0__1__0__0__0' '0__1__0__0__0__0' '1__0__0__0__0__0'}
+res_arr = {'conds_hitVcr_trTe1_2_3_4_0_0_0_0_trW' 'conds_hitVcr_trTe0_0_0_0_1_2_3_4_trW'};
+tr_str_arr = {'0__0__0__0__0__1' '0__0__0__0__1__0' '0__0__0__1__0__0' '0__0__1__0__0__0' '0__1__0__0__0__0' '1__0__0__0__0__0'}
 mask_str = 'roiSEPT09_MVPA*';
 
-colors = {};
+colors = {'g','r'};
 
-figure;
+f =figure;
 
-
-for tr = trs_to_use
-    fname = dir([res_dir '/' res_str tr_str{tr} '_' mask_str 'mat']);
-    res= load(fullfile(res_dir, fname.name)); 
-    
-    res = res.res;
-    
-    proc_res{tr} = CM_mvpaPostProc(res, [], 'ret', '',[]);
-    aucs(tr,:) = proc_res{tr}.auc
+for r = 1:length(res_arr)
+    res_str = res_arr{r};
+    for tr = trs_to_use
+        fname = dir([res_dir '/' res_str tr_str_arr{tr} '_' mask_str 'mat']);
+        res= load(fullfile(res_dir, fname.name));
         
+        res = res.res;
+        
+        proc_res{tr} = CM_mvpaPostProc(res, [], 'ret', '',[]);
+        auc(tr,:) = proc_res{tr}.auc
+        
+    end
+    
+    mean_auc = nanmean(auc,2);
+    ste_auc = nansem(auc,2);
+    
+    set(0, 'currentfigure', f);  %# for figures
+    errorbar3(trs_to_use,mean_auc',ste_auc',1,colors{r});
+    hold on;
+    plot(trs_to_use,mean_auc',colors{r}, 'LineWidth',1.5);
+end
 end
 
-plot(trs_to_use,nanmean(auc,2));
-
-errorbar3(trs_to_use,nanmean(auc,2),ste(auc,2),1,[.2 .6 1]);
-
-end
 
 
-    
-    
