cb6ae3783103c295710946b0a9f8383246bf940f
tyler boyd <jboydmeredith@gmail.com>
2014-10-03 17:19:30 -0700

    fix CM_run_and_report_mvpa arg passing

--- CM_run_and_report_mvpa.m
+++ CM_run_and_report_mvpa.m
@@ -1,4 +1,4 @@
 function CM_run_and_report_mvpa(subj_arr, xvalIterToReport, varargin)
-res = CM_run_mvpa_v4_tbm(subj_arr, '', varargin);
+res = CM_run_mvpa_v4_tbm(subj_arr, '', varargin{:});
 cd ('/Users/Jesse/fMRI/COUNTERMEASURES/Data/Functional/mvpa_results');
 CM_mvpaPostProc(res, xvalIterToReport, 'ret', 1, pwd, subj_arr);
