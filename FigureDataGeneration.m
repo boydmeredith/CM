5070458623c8ab311f0f160952d3090b318d069c
tyler boyd <jboydmeredith@gmail.com>
2014-11-11 13:25:37 -0800

    Add code for ex > cm analyses across trs
    In FigureDataGeneration, add code to train on trs 3-4 and test on trs
    1-6 in both explicit and countermeasures testing data

--- FigureDataGeneration.m
+++ FigureDataGeneration.m
@@ -1,4 +1,5 @@
 NSUBS = 24;
+subj_array  = [1,3:10,12:26];
 %generate data for across subject hit v cr classification masked by ex hit
 %> cr and cm hit > cr effects inclusively masked at the 2nd level across
 %the held-out 24 subjects.
@@ -22,3 +23,20 @@ assert(length(dir(theseResStr)) == (1+length(TRSTOTEST))*NSUBS);
 CM_batchMvpaPostProc(pwd,theseResStr,2,'ret',[],'acrossSs_trEXteCM_combinedFXMask_hitvcr',0,[5:8]);
 %analyze for EX runs
 CM_batchMvpaPostProc(pwd,theseResStr,2,'ret',[],'acrossSs_trEXteEX_combinedFXMask_hitvcr',0,[1:4]);
+
+% generate
+for iTrainTr = 3:4
+    for jTestTr = 1:6
+        trW_train = zeros(1,6);
+        trW_test = zeros(1,6);
+        trW_train(iTrainTr) = 1;
+        trW_test(jTestTr) = 1;
+        
+        CM_run_and_report_mvpa(subj_array,[],2,'trWeights_train',trW_train,...
+            'trWeights_test',trW_test, 'which_traintest',[1 1 1 1 2 2 2 2]);
+        
+        CM_run_and_report_mvpa(subj_array,[],[],'trWeights_train',trW_train,...
+            'trWeights_test',trW_test, 'which_traintest',[1 1 2 2 0 0 0 0]);
+
+    end
+end
