478abdd5ed2e9ba873c8f0f59e8354367759b784
Tyler Boyd-Meredith <jtboyd@stanford.edu>
2014-11-18 17:23:16 -0800

    Batch mvpa postproc now produces data frame for R
    The output is now built with R in mind.
    
    This could be improved upon by adding info about the classification, so
    that we don't have to figure it out based on the name in R later.

--- CM_batchMvpaPostProc.m
+++ CM_batchMvpaPostProc.m
@@ -23,13 +23,13 @@ fileNames = {dFN.name};
 dotFiles = cell2mat(cellfun(@(x) x(1)=='.', fileNames, 'UniformOutput', false));
 fileNames(find(dotFiles)) = []; %remove hidden files that are prepended with dots.
 
-for resf =1:length(fileNames)
-    display(['Now processing ' fileNames{resf} '...']);
-    thisRes =load(fileNames{resf},'-mat');
+for ires =1:length(fileNames)
+    display(['Now processing ' fileNames{ires} '...']);
+    thisRes =load(fileNames{ires},'-mat');
     isAcrossSubsResStruct = isfield(thisRes,'results');
     if isAcrossSubsResStruct
         res.subj{1}.penalty.nVox.weights.iter{1} = thisRes.results{1};
-        res.subj{1}.penalty.nVox.weights.expt{1}.saveName = fileNames{resf};
+        res.subj{1}.penalty.nVox.weights.expt{1}.saveName = fileNames{ires};
         res.subj{1}.penalty.nVox.weights.condensed_runs = thisRes.results{1}.condensed_runs;
         subjArray = 1;
     else
@@ -39,32 +39,43 @@ for resf =1:length(fileNames)
     if isempty(userSubjArray)
         subjArray = res.subjArray;
     end
-    [resB{resf} cvB{resf}] = CM_mvpaPostProc(res, xvalIterToReport,runs_to_report, task, plotit, fullfile(resDir,'figs'), subjArray);
-    resB{resf}.name = fileNames{resf};
-    cvB{resf}.name = fileNames{resf};
-    
-    toR.resname{resf} = resB{resf}.name;
-    toR.mean_auc(resf,:) = nanmean(resB{resf}.auc);
-    toR.sem_auc(resf,:) = nansem(resB{resf}.auc);
-    for s = allSubsArray
-        fn = sprintf('s%02d_auc',s);
-        if s > length(resB{resf}.auc)
-            toR.(fn)(resf,:) = nan;
-        else
-        toR.(fn)(resf,:) = resB{resf}.auc(s);
-        end
-    end
+    [resB{ires} cvB{ires}] = CM_mvpaPostProc(res, xvalIterToReport,runs_to_report, task, plotit, fullfile(resDir,'figs'), subjArray);
+    resB{ires}.name = fileNames{ires};
+    cvB{ires}.name = fileNames{ires};
+    resB{ires}.subjArray = subjArray;
 end
 
+nres = length(resB);
+idxToEdit = 0;
+for ires = 1:nres
+	nsubs = length(resB{ires}.subjArray);
+	for jsub = 1:nsubs
+		idxToEdit = idxToEdit+1;
+		toR.resname{idxToEdit} = resB{ires}.name;
+		toR.mean_auc(idxToEdit) = nanmean(resB{ires}.auc);
+		toR.sem_auc(idxToEdit) = nansem(resB{ires}.auc);
+		toR.subNo{idxToEdit} = sprintf('s%02',jsub);
+		if jsub > length(resB{ires}.auc)
+			toR.auc(idxToEdit);
+		end
+		toR.auc(ires,:) = resB{ires}.auc(jsub);
+         end
+ end
+ 
 fn = fieldnames(toR);
+
 out=['name';toR.resname'];
 
 for f = 2:length(fn)
     vals = toR.(fn{f});
-    assert(isfloat(vals));
-    out = horzcat(out,[fn{f}; num2cell(vals)]);
+    if(isfloat(vals))
+	    out = horzcat(out,[fn{f}; num2cell(vals)']);
+    else
+	    out = horzcat(out,[fn{f}; vals']);
+    end
+
 end
-out=horzcat(['xval iterations reported'; repmat({num2str(xvalIterToReport)},resf,1)], out);
+out=horzcat(['xval iterations reported'; repmat({num2str(xvalIterToReport)},idxToEdit,1)], out);
 
 if ~isempty(saveName)
     cell2csv(fullfile(resDir, ['aucSummary_' runs_to_report '_' saveName '.csv']), out, ',', 2000);
