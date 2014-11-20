e7e0e39be1a7d4c2ee4fb0bf359925a6b9c3cf3e
tyler boyd <jboydmeredith@gmail.com>
2014-11-20 15:38:15 -0800

    Comment batch mvpa post proc. reorder arg inputs
    Reorder argument inputs so that the most important come first. Specify
    default values, so that not all of the arguments need to be input, as
    the number has become rather cumbersome.
    
    Also change a few variable names, so that it is easier to inperpret the
    code.

--- CM_batchMvpaPostProc.m
+++ CM_batchMvpaPostProc.m
@@ -1,5 +1,5 @@
-function [resB cvB out] = CM_batchMvpaPostProc(classStr, resDir, xvalIterToReport, task, subjArray, saveName, plotit, runs_to_report)
-%function [resB cvB out] = CM_batchMvpaPostProc( classStr,resDir, xvalIterToReport, task, subjArray, saveName, plotit, runs_to_report)
+function [resB cvB out] = CM_batchMvpaPostProc(classStr, saveName, resDir, plotit, xvalIterToReport, runs_to_report,subjArray, task)
+%function [resB cvB out] = CM_batchMvpaPostProc(classStr, saveName, resDir, plotit, xvalIterToReport, runs_to_report,subjArray, task)
 %inputs:
 %<resDir> the directory containing the results mat files
 %<classSt>: default value is '*conds*". Filters mat files
@@ -7,64 +7,104 @@ function [resB cvB out] = CM_batchMvpaPostProc(classStr, resDir, xvalIterToRepor
 %<task>: default value is 'ret'
 
 toR = [];
-allSubsArray = [1 3:10 12:26];
-userSubjArray = subjArray;
-
 
 if isempty(classStr)
     classStr = '*conds*';
 end
-if isempty(task)
+if isempty(saveName)
+    saveName = classStr;
+end
+
+if nargin < 3 
+    resDir = pwd
+end
+if nargin < 4
+    plotit = 0
+end
+if nargin < 5
+    xvalIterToReport = []
+end
+if nargin < 6
+    runs_to_report = []
+end
+if nargin < 7
+    subjArray = []
+end
+if nargin < 8
     task = 'ret';
 end
 
+userSubjArray = subjArray;
+
+% get list of all files matching classStr and make sure there's something
+% there
 dFN = dir(fullfile(resDir, [classStr]));
 fileNames = {dFN.name};
 dotFiles = cell2mat(cellfun(@(x) x(1)=='.', fileNames, 'UniformOutput', false));
 fileNames(find(dotFiles)) = []; %remove hidden files that are prepended with dots.
-nfiles = length(fileNames)
-assert(nfiles>0);
-for ires =1:nfiles
-    display(['Now processing ' fileNames{ires} '...']);
-    thisRes =load(fileNames{ires},'-mat');
-    isAcrossSubsResStruct = isfield(thisRes,'results');
-    if isAcrossSubsResStruct
+nfile = length(fileNames)
+assert(nfile>0);
+
+% iterate through the files and get results
+for ifile =1:nfile
+    % tell the user what we're working on
+    display(['Now processing ' fileNames{ifile} '...']);
+    % load results
+    thisRes =load(fileNames{ifile},'-mat');
+    % If it is an across subject classification, get the res structure into
+    % the epected format
+    isAcrossSubs = isfield(thisRes,'results');
+    res.isAcrossSubsResStruct = isAcrossSubs;
+    if isAcrossSubs
         res.subj{1}.penalty.nVox.weights.iter{1} = thisRes.results{1};
-        res.subj{1}.penalty.nVox.weights.expt{1}.saveName = fileNames{ires};
+        res.subj{1}.penalty.nVox.weights.expt{1}.saveName = fileNames{ifile};
         res.subj{1}.penalty.nVox.weights.condensed_runs = thisRes.results{1}.condensed_runs;
         subjArray = 1;
     else
         res = thisRes.res;
     end
-    res.isAcrossSubsResStruct = isAcrossSubsResStruct;
+    
+    % if user did not specify subject array, use info in res struct
     if isempty(userSubjArray)
         subjArray = res.subjArray;
     end
-    [resB{ires} cvB{ires}] = CM_mvpaPostProc(res, xvalIterToReport,runs_to_report, task, plotit, fullfile(resDir,'figs'), subjArray);
-    resB{ires}.name = fileNames{ires};
-    cvB{ires}.name = fileNames{ires};
-    resB{ires}.subjArray = subjArray;
-    resB{ires}.expt = res.subj{1}.penalty.nVox.weights.expt{1};
+    % do the actual processing of this results struct
+    [resB{ifile} cvB{ifile}] = CM_mvpaPostProc(res, xvalIterToReport,runs_to_report, task, plotit, fullfile(resDir,'figs'), subjArray);
+    
+    % package the processed results with the name, subject array, and expt
+    % params used
+    resB{ifile}.name = fileNames{ifile};
+    cvB{ifile}.name = fileNames{ifile};
+    resB{ifile}.subjArray = subjArray;
+    resB{ifile}.expt = res.subj{1}.penalty.nVox.weights.expt{1};
 
 end
 
-nres = length(resB);
-idxToEdit = 0;
-fieldsToRecord = {'name','condNames','which_traintest','roiName','num_results_iter','scramble','trWeights','trWeights_train','trWeights_test'}
+% now that we have a structure of results, we can put iterate through them
+% and input them into a data frame format to send to R
+fieldsToRecord = {'name','condNames','which_traintest','roiName','num_results_iter','scramble','trWeights','trWeights_train','trWeights_test'};
+nres = length(resB); % number of results to record
+idxToEdit = 0; % counter for index in data frame
+
+%iterate through each result
 for ires = 1:nres
 	nsubs = length(resB{ires}.subjArray);
+    %iterate through each subject 
 	for jsub = 1:nsubs
 		idxToEdit = idxToEdit+1;
+        % record result values of interest
 		toR.name{idxToEdit} = resB{ires}.name;
 		toR.mean_auc(idxToEdit) = nanmean(resB{ires}.auc);
 		toR.sem_auc(idxToEdit) = nansem(resB{ires}.auc);
 		toR.subNo{idxToEdit} = sprintf('s%02d',jsub);
+        %make sure that this subject is actually in the subject array
         if jsub > length(resB{ires}.auc)
             toR.auc(idxToEdit)='nan';
 		end
 		toR.auc(idxToEdit) = resB{ires}.auc(jsub);
         toR.xvalIterReported{idxToEdit} = num2str(xvalIterToReport);
         
+        %record values of interest from expt params
         for kfield = 1:length(fieldsToRecord)
             fieldname = fieldsToRecord{kfield}
             if isfield(resB{ires}.expt, fieldname)
@@ -76,13 +116,16 @@ for ires = 1:nres
             toR.(fieldname){idxToEdit} = value;
         end
     end
- end
+end
+ 
+% take the toR struct and turn into a big matrix that can actually be
+% turned into a csv
  
 fn = fieldnames(toR);
-
 out=['name';toR.name'];
+fn(strcmp(fn,'name'))=[];
 
-for f = 2:length(fn)
+for f = 1:length(fn)
     vals = toR.(fn{f});
     if(isfloat(vals))
 	    out = horzcat(out,[fn{f}; num2cell(vals)']);
@@ -91,6 +134,8 @@ for f = 2:length(fn)
     end
 
 end
+
+%turn the big matrix into a csv
 
 if ~isempty(saveName)
     cell2csv(fullfile(resDir, ['aucSummary_' runs_to_report '_' saveName '.csv']), out, ',', 2000);
