5884af4efce4edf2ac264799221c235b5acafb5e
tyler boyd <jboydmeredith@gmail.com>
2014-12-05 15:49:39 -0800

    mend

--- base_matlab.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#$ -N matlab_CMMVPA
-
-module load MATLAB-R2012b
-matlab -r "cd ~/cm/Data/Functional/mvpa_results; CM_batchMvpaPostProc(pwd, 'conds*1_2_3_4*',[],'')"
--- run_matlab.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-qsub -l h_vmem=10G -v  /hsgs/projects/awagner/jtboyd/CM_ret/Scripts  base_matlab.bash
-
-
