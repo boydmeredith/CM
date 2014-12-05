41487cb9e3448edb1891a107fb9caa57272c8b4d
tyler boyd <jboydmeredith@gmail.com>
2014-12-05 15:49:12 -0800

    remove uneeded bash scripts written for proclus

--- cm_job_wrap_test.bash
+++
@@ -1,7 +0,0 @@
-module load MATLAB-R2012b
-for trW in "[0 1 0 0 0 0]"
-do
-foo="[],'','trWeights',${trW}"
-#bash cm_job.bash ${foo}
-qsub -l h_vmem=10G -v foo="[],'','trWeihts',${trW}" -cwd cm_job.bash "CM_run_mvpa_v4_tbm([],'','trWeights',${trW})"
-done
