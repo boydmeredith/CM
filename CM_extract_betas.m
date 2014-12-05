1ffaa9730d4a00911d3ba18db5d21ec0658172ad
tyler boyd <jboydmeredith@gmail.com>
2014-12-05 15:50:13 -0800

    Add beta extraction script for cm
    This script takes voxel coordinates and extracts the parameter estimates
    for each condition name in the onsets file associated with the extracted
    betas.

---
+++ CM_extract_betas.m
@@ -0,0 +1,69 @@
+function betas_out = CM_extract_betas(clus_coords)
+% in the intended use case <clus_coords> is created using spm's eigenvariate
+% function, which outputs a variable <xY.XYZmm>
+
+USING_TD_DERIVS=1;
+% experiment specific parameters
+expt_name = 'CM';
+fmri_dir = '/Users/Jesse/fMRI/COUNTERMEASURES/Data/Functional';
+analysis_name = 'Results_model2';
+subj_array = [1,3:10,12:26];
+
+display('Warning: this function will not correct for instances where subjects do not share conditions');
+
+% get count of subjects
+nSubj = length(subj_array); 
+
+% ensure that clus_coords is properly oriented
+if size(clus_coords,2) ~= 3
+    display('reshaping input voxel coordinates');
+    clus_coords = clus_coords';
+end
+[nRows_vox, nCols_vox] = size(clus_coords);
+assert(nCols_vox == 3);
+
+
+for vox = 1:nRows_vox
+    
+    fprintf('\n Pulling betas from voxel %d of %d ',...
+               vox, nRows_vox);
+    
+    this_vox = clus_coords(vox,:);
+    
+    for subj = 1:nSubj
+        
+        this_subjname = sprintf('%s%03d', expt_name, subj_array(subj));
+               
+        this_subj_analysis_dir = fullfile(fmri_dir, this_subjname, analysis_name, 'Test');
+        
+        this_subj_conds = load(fullfile(this_subj_analysis_dir,  sprintf('CM%03d_Test_Onsets.mat',subj_array(subj))), 'names');
+        
+        nConds = length(this_subj_conds.names);
+        
+        for cond = 1:nConds
+            
+            this_condname = this_subj_conds.names{cond};
+            
+            %choose every third beta, assuming that we're using time and
+            %dispersion derivatives
+            if USING_TD_DERIVS
+                this_beta_fname = fullfile(this_subj_analysis_dir, sprintf('beta_%04d.img',1+(cond-1)*3));
+            else
+                this_beta_fname = fullfile(this_subj_analysis_dir, sprintf('beta_%04d.img',cond));
+            end
+            this_beta = get_vox(this_beta_fname, this_vox);
+            
+            betas_out.(this_condname)(subj,vox) = this_beta;
+            
+            if isnan(this_beta)
+                error('beta was returned as nan');
+            end
+            
+
+        end
+        
+    end
+    
+end
+            
+
